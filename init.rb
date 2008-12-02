211aa4675e1d4d5b9f74c91f545e06ce1d6135b8
root <horaci@gmail.com>
2008-12-02 14:56:37 +0100

    bad copy/paste :)

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 # Include hook code here
 
 unless Rails::VERSION::MAJOR == 2 && Rails::VERSION::MINOR >= 2
-  raise "This version of ActiveScaffold requires Rails 2.2 or higher.  Please use an earlier version."
+  raise "This version of acts-as-i18n-ui requires Rails 2.2 or higher.  Please use an earlier version."
 end
 
 
