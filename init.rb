2716bdadacada8557c502d97270f0b3bf9a0632b
root <horaci@gmail.com>
2008-12-02 14:52:04 +0100

    changed install scripts

--- init.rb
+++ init.rb
@@ -1,5 +1,17 @@
 # Include hook code here
 
+unless Rails::VERSION::MAJOR == 2 && Rails::VERSION::MINOR >= 2
+  raise "This version of ActiveScaffold requires Rails 2.2 or higher.  Please use an earlier version."
+end
+
+
+begin
+  require File.dirname(__FILE__) + '/install_assets'
+rescue
+  raise $! unless Rails.env == 'production'
+end
+
+
 ActionController::Base.class_eval do
   include ActsAsI18nUI
 end
